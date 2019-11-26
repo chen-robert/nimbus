c0b260aba3a87777ef25e40e9264db7cb01c5352
Robert <robertchen@live.com>
2019-11-26 06:40:37 +0000

    Added ability to buy stocks

--- index.js
+++ index.js
@@ -16,28 +16,57 @@ const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync(__dirname + "/ext/db.json");
 const db = low(adapter);
 
-db.defaults({ users: {}, sales: [], features: [] }).write();
+db.defaults({ users: {}, sales: {}, features: [] }).write();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+if(false) {
+  const users = Object.keys(db.get("users").value());
+
+  users.forEach(user => {
+    const uname = db.get("users").get(user).get("username").value();
+
+    if(uname === undefined) {
+      db.get("users").get(user).set("auth", false).write();
+      return;
+    }
+
+    db.get("users")
+      .get(user)
+      .set("auth", true)
+      .set("stock", {
+        [uname]: config.startingStock
+      })
+      .set("money", 20)
+      .write();
+  });
+}
+
 const helpMsgs = {
   help: config.prefix + "help",
   auth: config.prefix + "auth [codeforces username]. e.g. `" + config.prefix + "auth gamesterrex`",
   verify: config.prefix + "verify",
-  list: config.prefix + "list"
+  list: config.prefix + "list | Lists what stocks you own",
+  sales: config.prefix + "sales | List what stocks you can purchase",
+  sell: config.prefix + "sell [stock] [amt] | Sell [amt] of [stock]",
+  buy: config.prefix + "buy [stock] [amt]"
 }
 
 const cache = {};
 const ratings = ["newbie", "pupil", "specialist", "expert", "candidate master", "master", "international master", "grandmaster", "international grandmaster", "legendary grandmaster"]
 const getVal = async username => {
+  if(cache[username]) return cache[username];
+
   const data = JSON.parse(await request(`https://codeforces.com/api/user.info?handles=${username}`)).result[0];
 
-  return data.rating === undefined ? 
+  cache[username] = data.rating === undefined ? 
     0 
     : 
     Math.pow(1.2, ratings.indexOf(data.rank)) * Math.max(0, data.rating) / 100;
+
+  return cache[username];
 }
 
 client.on('message', async msg => {
@@ -59,7 +88,7 @@ client.on('message', async msg => {
     if(cmd === "help") {
       msg.channel.send("```" + Object.values(helpMsgs).join("\n") + "\n```");
     } else if (cmd === "feature") {
-      if(msg.author.id === "292493700427415554") return msg.channel.send(":(");
+      if(msg.author.id === "292493700427415554" || msg.author.id === "530157531314520066") return msg.channel.send(":(");
       console.log(msg.author.id + " just did a feature request");
       const msgs = parts.join(" ");
 
@@ -116,7 +145,7 @@ client.on('message', async msg => {
           .write();
         }
       });
-    } else if (["list", "sell", "sales"].includes(cmd)) {
+    } else if (["list", "sell", "sales", "buy"].includes(cmd)) {
       if(verified) {
         const stocks = db.get("users").get(uid).get("stock").value();
         const bal = db.get("users").get(uid).get("money").value();
@@ -133,28 +162,59 @@ client.on('message', async msg => {
           msg.channel.send(ret);
         } else if (cmd === "sell") {
           const name = parts[0];
-          const amt = parts[1];
+          const amt = Math.floor(+parts[1]);
+
+          if(!name || !amt) return msg.channel.send(helpMsgs.sell);
 
           if(!stocks[name]) return msg.channel.send("Error, you currently do not own any stock of type " + name);
           
           const curr = stocks[name];
           if(curr < amt) return msg.channel.send("Error, you only have " + curr + " stocks");
+          if(amt <= 0) return msg.channel.send("You can't sell " + amt + " stock");
           
           const value = await getVal(name);
           db.get("users").get(uid).get("stock").set(name, curr - amt).write();
           db.get("users").get(uid).set("money", value * amt + bal).write();
+          
+          if(!db.get("sales").has(name).value()) db.get("sales").set(name, 0).write();
 
-          db.get("sales").push({name, amt}).write();
+          db.get("sales").set(name, db.get("sales").get(name).value() + amt).write();
 
           return msg.channel.send("Successfully sold `" + amt + "` of stock `" + name + "`");
         } else if(cmd === "sales") {
           let ret = "Stock Availble:\n```";
-          for(const {name, amt} of db.get("sales").value()) {
-            ret += name + " : " + amt + "\n";
+          for(const [name, amt] of Object.entries(db.get("sales").value())) {
+            if(amt === 0) continue;
+
+            const value = await getVal(name);
+            ret += name + " : " + amt + " each at $" + value.toFixed(2) + "\n";
           }
           ret += "\n```";
 
           return msg.channel.send(ret);
+        } else if(cmd === "buy") {
+          const name = parts[0];
+          const amt = Math.floor(+parts[1]);
+
+          const sales = db.get("sales").value();
+
+          if(!name || !amt) return msg.channel.send(helpMsgs.buy);
+          if(amt <= 0) return msg.channel.send("You can't sell " + amt + " stock");
+          if(!sales[name]) return msg.channel.send("Error, no stock of type " + name + " is currently for sale");
+
+          const curr = sales[name];
+
+          const value = await getVal(name);
+          if(curr < amt) return msg.channel.send("Error, only " + curr + " stocks for sale");
+          if(value * amt > bal) return msg.channel.send("You cannot afford to purchase this much stock");
+
+          const currStockAmt = db.get("users").get(uid).get("stock").get(name).value() || 0;
+          db.get("users").get(uid).get("stock").set(name, currStockAmt + amt).write();
+          db.get("users").get(uid).set("money", bal - value * amt).write();
+
+          db.get("sales").set(name, curr - amt).write();
+
+          return msg.channel.send("Successfully purchased `" + amt + "` of stock `" + name + "`");
         }
       } else {
         msg.channel.send("Please verify your codeforces account first with " + helpMsgs.auth);
@@ -164,8 +224,5 @@ client.on('message', async msg => {
     }
   }
 });
-
-
-
 
 client.login(process.env.API_KEY);
